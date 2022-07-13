26649f6a52152844f12ac79a9cd2a7704d590647
luk3mn <lucasrenan2015@gmail.com>
2022-07-13 10:26:01 -0300

    Realizando o update, delete e o mecanismo de busca por nome de item

--- item/urls.py
+++ item/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ItemCreateView, ListaItemView # importa a classe 'ListaItemView' da views do app 'item' para renderizar a página de itens do site
+from .views import ItemCreateView, ListaItemView, UpdateItemView, DeleteItemView # importa a classes do CRUD da views do app 'item' para realizar as tarefas no DB
 
 urlpatterns = [
-    path('', ListaItemView.as_view(), name='item.index'),
-    path('novo/', ItemCreateView.as_view(), name='item.novo'),
+    path('', ListaItemView.as_view(), name='item.index'), # rota de leitura dos dados (listagem)
+    path('novo/', ItemCreateView.as_view(), name='item.novo'), # rota de criação
+    path('editar/<int:pk>', UpdateItemView.as_view(), name='item.editar'), # rota de edição
+    path('remover/<int:pk>', DeleteItemView.as_view(), name='item.remover')
 ]
--- item/views.py
+++ item/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView, CreateView # componente para listagem
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView # componente do CRUD
 from .models import Item # importa o modelo criado para a tebela Item no db
 from .forms import ItemForm
 
-# Construção da classe para consultar os itens no db e listar com auxilo do 'ListView'
+# (READING) Construção da classe para consultar os itens no db e listar com auxilo do 'ListView'
 class ListaItemView(ListView):
     model = Item
     queryset = Item.objects.all().order_by('nome_item') # consulta todos os registro da tabela ordenado pelo 'nome_item'
 
+    # mecanismo de busca
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        buscar_item = self.request.GET.get('pesquisar') or None # pega a requisição pela url (GET) com o name informado no formulario
+
+        # verifica se retorna algo pelo metodo GET (url)
+        if buscar_item:
+            # aplica o filtro passando o nome do item informado na models
+            queryset = queryset.filter(nome_item__contains=buscar_item)
+
+        return queryset
+
+# (CREATE)
 class ItemCreateView(CreateView):
     model = Item # indica o modelo de referencia
     form_class = ItemForm # indica a classe que manipula o formalário
     success_url = '/itens/' # redireciona para 'itens'
+
+# (UPDATE)
+class UpdateItemView(UpdateView):
+    model = Item # referencia a tabela (classe da model) do db
+    form_class = ItemForm # passa os dados do formulario para para 'form_class'
+    success_url = '/itens/' # caso obtenho sucesso, faz o redirecionamento
+
+# (DELETE)
+class DeleteItemView(DeleteView):
+    model = Item # referencia a model
+    success_url = '/itens/' # faz o redirecionamento em caso de sucesso
