54f0c53b4737219ac871c00e2f6ec195cfe89d59
luk3mn <lucasrenan2015@gmail.com>
2022-07-26 11:06:01 -0300

    Refazendo as rotas de remoção de itens e preparando um mecanismo para verificar se o item foi marcado como comprado

--- item/urls.py
+++ item/urls.py
@@ -6,14 +6,8 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', views.home, name="item.home"),
     path('sobre/', views.sobre, name="item.about"),
-    path('adicionar/', views.adicionar_item, name="item.adicionar"),
+    path('adicionar/<int:id_usuario>/', views.adicionar_item, name="item.adicionar"),
     path('atualizar/', views.atualizar_item, name="item.atualizar"),
-    path('remover/', views.remover_item, name="item.remover"),
-    # na url de itens é utilizado o login required passando o segundo parâmetro indicando o caminho
-    # path('', login_required(ListaItemView.as_view(), login_url='/usuarios/login/'), name='item.index'), # rota de leitura dos dados (listagem)
-    # em 'novo', 'editar' e 'remover' foi utilizado o 'LoginRequiredMixin' que faz o mesmo que o 'login_required', porem em Generic Views
-    # path('novo/', ItemCreateView.as_view(), name='item.novo'), # rota de criação
-    # path('listar/<int:id_usuario>/', views.listar, name='item.listar'),
-    # path('editar/<int:pk>', UpdateItemView.as_view(), name='item.editar'), # rota de edição
-    # path('remover/<int:pk>', DeleteItemView.as_view(), name='item.remover')
+    path('remover/<int:id_item>/', views.remover_item, name="item.remover"),
+    path('marcar/<int:id_item>/', views.verificar_item, name="item.verificar"),
 ]
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-
+from datetime import date
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView # componente do CRUD
 from .models import Item # importa o modelo criado para a tebela Item no db
 from .forms import ItemForm
@@ -12,70 +12,48 @@ from django.contrib.auth.models import User
 def home(request):
     id_usuario = request.user.id # pega o id do usuário logado
     itens = Item.objects.filter(usuario=id_usuario) # consulta todos os itens associado ao id do usuario logado
-    return render(request, 'home.html', {'itens':itens, 'id_usuario':id_usuario}) # retorna para a home incluindo na lista os itens e o id do usuario logado
+    return render(request, 'home.html', {'data':date.today,'itens':itens, 'id_usuario':id_usuario}) # retorna para a home incluindo na lista os itens e o id do usuario logado
 
 def sobre(request):
     return render(request, 'about.html')
 
 # (CREATE)
-def adicionar_item(request):
-    id_usuario = request.user.id # pega o id do usuário logado
+def adicionar_item(request, id_usuario):
+    # id_usuario = request.user.id # pode pegar o id diretamente, mas nesse caso está puxando pelo GET vindo da url
     usuario = get_object_or_404(User, id=id_usuario)
-    if request.method == "POST":
-        item = request.POST.get('item')
+    if request.method == "POST": # verifica se o método de requisição é POST
+        item = request.POST.get('item') # pega o nome do item vindo do formulário pelo POST
 
+        # instancia a model do banco de dados passando para ela o id do usuario (FK) e o nome do item
         adicionar_item = Item(usuario=usuario, nome_item=item)
-        adicionar_item.save()
+        adicionar_item.save() # salva no banco de dados
 
-        return redirect('/')
-        # return redirect(reverse('item.home', args=[id_usuario]))
+    return redirect('/')
 
 # (UPDATE)
 def atualizar_item(request):
     return HttpResponse('ALTERAR NOME DO ITEM')
 
 # (DELETE)
-def remover_item(request):
-    return HttpResponse('MARCAR ITEM COMO CONCLUÍDO')
+def remover_item(request, id_item):
+    item = get_object_or_404(Item, id=id_item)
+    if id_item == item.id:
+        marcar_item = Item(id=id_item) # remove o item do banco
+        marcar_item.save() # salva no banco de dados
+        return redirect('/')
+    else:
+        return HttpResponse("Não existe")
 
-# # (READING) Construção da classe para consultar os itens no db e listar com auxilo do 'ListView'
-# class ListaItemView(ListView):
-#     model = Item
-#     queryset = Item.objects.all().order_by('data_adicao') # consulta todos os registro da tabela ordenado pelo 'nome_item'
+def verificar_item(request, id_item):
+    # item = Item.objects.get(id=id_item) # primeira forma de acessar os valores do banco
+    item = get_object_or_404(Item, id=id_item) # segunda forma de acessar os valores do banco
+    item.checado = True # altera o valor do campo 'checado' para True
+    item.save() # salva no banco
 
-#     # mecanismo de busca
-#     def get_queryset(self):
-#         queryset = super().get_queryset()
-#         queryset = queryset.filter(usuario=self.request.user) # busca os itens associados ao usuario logado
-#         buscar_item = self.request.GET.get('pesquisar') or None # pega a requisição pela url (GET) com o name informado no formulario
+    return redirect('/') # redireciona para o index
 
-#         # verifica se retorna algo pelo metodo GET (url)
-#         if buscar_item:
-#             # aplica o filtro passando o nome do item informado na models
-#             queryset = queryset.filter(nome_item__contains=buscar_item)
-
-#         return queryset
-
-# # (CREATE)
-# class ItemCreateView(LoginRequiredMixin, CreateView):
-#     login_url = '/usuarios/login/'
-#     model = Item # indica o modelo de referencia
-#     form_class = ItemForm # indica a classe que manipula o formalário
-#     success_url = '/itens/' # redireciona para 'itens'
-
-#     def form_valid(self, form):
-#         form.instance.usuario = self.request.user
-#         return super().form_valid(form)
-
-# # (UPDATE)
-# class UpdateItemView(LoginRequiredMixin, UpdateView):
-#     login_url = '/usuaios/login/' # -> Para
-#     model = Item # referencia a tabela (classe da model) do db
-#     form_class = ItemForm # passa os dados do formulario para para 'form_class'
-#     success_url = '/itens/' # caso obtenho sucesso, faz o redirecionamento
-
-# # (DELETE)
-# class DeleteItemView(LoginRequiredMixin, DeleteView):
-#     login_url = '/usuarios/login/'
-#     model = Item # referencia a model
-#     success_url = '/itens/' # faz o redirecionamento em caso de sucesso
+# ANOTAÇÔES:
+# - Verificar a opção de remover
+# - Add a opção de edição
+# - Fazer as validações
+# - Melhorar a visualização da lista
