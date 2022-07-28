7738f5ca56864c0a54a3730b326cc4dfa6cadab0
luk3mn <lucasrenan2015@gmail.com>
2022-07-28 11:04:43 -0300

    ativando o static_root

--- project/settings.py
+++ project/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'templates/static'),
 )
 
-# STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join('static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
